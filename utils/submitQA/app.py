216969e91750ca5032666b5623a78e4d587a797a
Mao Tian <maotian@redwpedadm25582.ad.medctr.ucla.edu>
2024-11-10 23:17:01 -0800

    add doi to jsonl and scripts

--- utils/submitQA/app.py
+++ utils/submitQA/app.py
@@ -16,19 +16,21 @@ def submit_qa():
     question = request.form['question']
     answer = request.form['answer']
     pmid = request.form['pmid']
+    doi = request.form['doi']
 
     # Create the dictionary to be saved as a JSON object
     qa_data = {
         "question": question,
         "answer": answer,
-        "pmid": pmid
+        "pmid": pmid,
+        "doi": doi
     }
 
     # Write the data to the JSONL file
     with open(FILE_PATH, 'a') as file:
         file.write(json.dumps(qa_data) + '\n')
 
-    return jsonify({'status': 'success', 'message': 'Q&A with PMID saved successfully!'})
+    return jsonify({'status': 'success', 'message': 'Q&A with PMID and DOI saved successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
