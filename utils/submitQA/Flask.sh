ad59225237532b5fd0fdaf7a80e53e857f6735f2
Mao Tian <maotian@Maos-Laptop.local>
2024-11-12 10:39:48 -0800

    add category and extra examples

--- utils/submitQA/Flask.sh
+++ utils/submitQA/Flask.sh
@@ -1,2 +1,3 @@
 source qadatabase/bin/activate
+cd /Users/maotian/Documents/Project/Project-GPT/iTRI-GPT/utils/submitQA
 python app.py
--- utils/submitQA/app.py
+++ utils/submitQA/app.py
@@ -17,20 +17,22 @@ def submit_qa():
     answer = request.form['answer']
     pmid = request.form['pmid']
     doi = request.form['doi']
+    category = request.form['category']
 
     # Create the dictionary to be saved as a JSON object
     qa_data = {
         "question": question,
         "answer": answer,
         "pmid": pmid,
-        "doi": doi
+        "doi": doi,
+        "category": category
     }
 
     # Write the data to the JSONL file
     with open(FILE_PATH, 'a') as file:
         file.write(json.dumps(qa_data) + '\n')
 
-    return jsonify({'status': 'success', 'message': 'Q&A with PMID and DOI saved successfully!'})
+    return jsonify({'status': 'success', 'message': 'Q&A with PMID, DOI, and Category saved successfully!'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
