520a9d07806cf2b63ac7dc53b46fc83b7b0ac628
Mao Tian <maotian@redwdomadm21552.ad.medctr.ucla.edu>
2025-01-03 22:09:23 -0800

    update jsonl and gitignore

--- utils/submit_QA_sample/Flask.sh
+++ utils/submit_QA_sample/Flask.sh
@@ -1,5 +1,5 @@
-source qadatabase/bin/activate
-cd /Users/maotian/Documents/Project/Project-GPT/iTRI-GPT/utils/submitQA
+source ~/qadatabase/bin/activate
+cd /Users/maotian/Documents/Project/Project-GPT/iTRI-GPT/utils/submit_qa_sample
 python app.py \
     --host 127.0.0.1 \
     --port 8080 \
