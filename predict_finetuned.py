3bbb7941d67c371205ccb849b0092fc5c362050d
Liu-Qiming <qliu0831@gmail.com>
2025-01-05 21:17:33 -0800

    rollback

--- predict_finetuned.py
+++ predict_finetuned.py
@@ -12,14 +12,11 @@ def main():
     parser.add_argument("--model_name", type=str, default=conf.model_name, help="Name of the model to use")
     args = parser.parse_args()
 
-    # Automatically default to 'cuda' if available, else fallback to 'cpu'
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {device}")
 
-    # Initialize the model
-    model = ItriModel(args.model_name, conf.adapter_path)
+    model = ItriModel("meta-llama/Llama-3.2-1B", conf.adapter_path)
 
-    # Initialize the PromptManager
     prompt_manager = PromptManager()
 
     try:
@@ -27,24 +24,15 @@ def main():
         # queries = load_jsonl_as_dict(conf.QA_data_path)[0]["question"]
         context_list = load_jsonl_as_dict(conf.QA_data_path)
 
-        # for ctx in context_list:
-        #     # Render and generate answers
-        #     print("Generating QA set based on the abstracts")
-        #     # Render the prompt using PromptManager
-        #     prompt = prompt_manager.render_prompt("llama3.2.j2", {"context": ctx["abstract"]})
-        #     answer = model.generate(prompt)
+        for ctx in context_list:
+            # Render and generate answers
+            print("Generating QA set based on the abstracts")
+            # Render the prompt using PromptManager
+            prompt = prompt_manager.render_prompt("llama3.2.j2", {"context": ctx["abstract"]})
+            answer = model.generate(prompt)
 
-        #     # Print results
-        #     # print(f"Query: {query}")
-        #     # print(f"Context: {context}\n")
-        #     # print("---------------------------------")
-        #     print(f"Answer: {answer}\n")
-        #     print("---------------------------------")
-        prompt = prompt_manager.render_prompt("llama3.2.j2", {"abstract": context_list[0]["abstract"]})
-        answer = model.generate(prompt)
-        print(answer)
-        # Save the model after generation
-        # model.save_model(base_save_path="./models")
+            print(f"Answer: {answer}\n")
+            print("---------------------------------")
 
     except FileNotFoundError as e:
         print(f"Error: {e}")
